dad883fad42f64ff8438081831e9ea2835740321
= <=>
2019-11-18 20:31:39 +0700

    chuyen thanh php

--- neha/index.php
+++ neha/index.php
@@ -56,7 +56,7 @@
                         <div class="row">
                             <div class="col-lg-6 col-md-6 col-6">
                                 <div class="logo">
-                                    <a href="index.html"><img src="assets/img/logo/logo.png" alt="" /></a>
+                                    <a href="index.php"><img src="assets/img/logo/logo.png" alt="" /></a>
                                 </div>
                             </div>
                             <div class="col-lg-6 col-md-6 col-6">
