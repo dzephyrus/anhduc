e3eae97cf894b60f597f01f0da56368997f7125b
longtvph08895 <57783272+longtvph08895@users.noreply.github.com>
2019-12-10 09:44:07 +0700

    slide

--- trangchu.php
+++ trangchu.php
@@ -263,45 +263,50 @@
                 <!--long slider-->
                 <div class="container" >
 					
-					<div id="myCarousel" class="carousel slide" data-ride="carousel">
-						
+				 <div id="myCarousel" class="carousel slide" data-ride="carousel">
 					<!-- Indicators -->
-						<ol class="carousel-indicators">
-						  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-						  <li data-target="#myCarousel" data-slide-to="1"></li>
-						  
-						</ol>
-
-						<!-- Wrapper for slides -->
-						
-						<div class="carousel-inner" style="max-width:100%; height:650px;">
-							
-						  <div class="item active" >
-							  <?php
-						$sql = "select * from slide where id_slide = 2";
-						$kqslide = $conn->query($sql);
-						foreach($kqslide as $key=>$value){
-					
+					<ol class="carousel-indicators">
+						<?php
+							$sql = "select * from slide where title='on'";
+							$kq = $conn -> query($sql);
+							$stt= 0;
+							foreach($kq as $key => $row){
+								?>
+									  <li data-target="#myCarousel" data-slide-to="<?php echo $stt; ?>"<?php if($stt==0){echo " class='active' ";}?>></li>
+								<?php
+								$stt ++;
+							}
 						?>
-							<img src="image/<?=$value['image_slide']?>" alt="" style ="width: 100%; height: 550px;" >
-							  <?php }
-						?>
-						  </div>
+					</ol>
 
-						  
-						  
-						</div>
-              
-						<!-- Left and right controls -->
-						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
-						  <span class="glyphicon glyphicon-chevron-left"></span>
-						  <span class="sr-only">Previous</span>
-						</a>
-						<a class="right carousel-control" href="#myCarousel" data-slide="next">
-						  <span class="glyphicon glyphicon-chevron-right"></span>
-						  <span class="sr-only">Next</span>
-						</a>
-						 
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner">
+						<?php
+							$sql = "select * from slide where title = 'on'";
+							$kq=$conn -> query($sql);
+							$stt =0;
+							foreach($kq as $key => $row){
+						?>
+						 <div class="item <?php  if($stt==0){echo 'active'; }?>">
+							<img src="image/<?php echo $row['image_slide'] ?>" style=" height: 600px; width:100%;">
+						 </div>
+						<?php
+							$stt ++;
+							}
+
+						?>
+
+							</div>
+
+							<!-- Left and right controls -->
+							<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							  <span class="glyphicon glyphicon-chevron-left"></span>
+							  <span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							  <span class="glyphicon glyphicon-chevron-right"></span>
+							  <span class="sr-only">Next</span>
+							</a>
 					  </div>
 					
 				</div><br>
